b2ea3bb2e51c73229bd9fc67011a6ee334b3d196
DmitriySolomatin <solomatin@megavega.ru>
2022-10-10 11:57:39 +0400

    remove RootController

--- src/main/java/ru/kata/spring/boot_security/demo/configs/MvcConfig.java
+++ src/main/java/ru/kata/spring/boot_security/demo/configs/MvcConfig.java
@@ -7,7 +7,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
-        //Зачем этот класс вообще нужен?
+        //Аналоги компонентов @Controller, если нужно просто отдать статику?
+        registry.addViewController("/").setViewName("index");
 //        registry.addViewController("/user").setViewName("user");
 //        registry.addViewController("/admin").setViewName("admin");
     }

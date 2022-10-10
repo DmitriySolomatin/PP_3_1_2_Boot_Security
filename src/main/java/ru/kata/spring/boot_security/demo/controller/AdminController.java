b9a9a74f4bbce88fb52557666b2c6a865b8edf48
DmitriySolomatin <solomatin@megavega.ru>
2022-10-10 11:51:34 +0400

    add RoleService interface

--- src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
@@ -1,12 +1,12 @@
 package ru.kata.spring.boot_security.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 import ru.kata.spring.boot_security.demo.model.User;
 import ru.kata.spring.boot_security.demo.service.RoleService;
+import ru.kata.spring.boot_security.demo.service.RoleServiceImp;
 import ru.kata.spring.boot_security.demo.service.UserService;
 import ru.kata.spring.boot_security.demo.service.UserServiceImp;
 

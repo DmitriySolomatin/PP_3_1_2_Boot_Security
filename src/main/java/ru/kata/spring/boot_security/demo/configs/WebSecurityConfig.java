163b1cc4779305d0f29b9bd69e37927ac8a93ec2
DmitriySolomatin <solomatin@megavega.ru>
2022-10-21 20:53:01 +0400

    upgrade to last version spring

--- src/main/java/ru/kata/spring/boot_security/demo/configs/WebSecurityConfig.java
+++ src/main/java/ru/kata/spring/boot_security/demo/configs/WebSecurityConfig.java
@@ -3,47 +3,33 @@ package ru.kata.spring.boot_security.demo.configs;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import ru.kata.spring.boot_security.demo.service.UserService;
 import ru.kata.spring.boot_security.demo.service.UserServiceImp;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
+public class WebSecurityConfig {
     private final SuccessUserHandler successUserHandler;
-    private final UserServiceImp userService;
+    private final UserService userService;
 
     public WebSecurityConfig(SuccessUserHandler successUserHandler, UserServiceImp userService) {
         this.successUserHandler = successUserHandler;
         this.userService = userService;
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .csrf().disable()
-                //Запрашивать авторизацию
-                .authorizeRequests()
-                //Доступ открыт всем
-                .antMatchers("/", "/index", "/registration", "/api/registration", "/css/**", "/js/**").permitAll()
-                //Если есть роль
-                .antMatchers("/api/current", "/api/adult").hasAnyRole("USER", "ADMIN")
-                .antMatchers("/admin", "/admin/**", "/api/**").hasRole("ADMIN")
-                .antMatchers("/user", "/user/**").hasAnyRole("USER", "ADMIN")
-                //Все остальные страницы требуют авторизацию
-                .anyRequest().authenticated()
-                .and()
-                //Разрешить доступ к форме логина для всех и применить хендлер
-                .formLogin().successHandler(successUserHandler)
-                .permitAll()
-                .and()
-                //Логаут разрешен всем
-                .logout()
-                .permitAll();
-    }
 
     // аутентификация inMemory
 //    @Bean
@@ -64,9 +50,40 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
-    @Autowired
-    protected void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userService).passwordEncoder(bCryptPasswordEncoder());
+    @Bean
+    public AuthenticationManager authManager(HttpSecurity http, BCryptPasswordEncoder bCryptPasswordEncoder, UserDetailsService userDetailService)
+            throws Exception {
+        return http.getSharedObject(AuthenticationManagerBuilder.class)
+                .userDetailsService(userDetailService)
+                .passwordEncoder(bCryptPasswordEncoder)
+                .and()
+                .build();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.
+                csrf().disable()
+                //Запрашивать авторизацию
+                .authorizeRequests()
+                //Доступ открыт всем
+                .antMatchers("/", "/index", "/registration", "/api/registration", "/css/**", "/js/**").permitAll()
+                //Если есть роль
+                .antMatchers("/api/current", "/api/adult").hasAnyRole("USER", "ADMIN")
+                .antMatchers("/admin", "/admin/**", "/api/**").hasRole("ADMIN")
+                .antMatchers("/user", "/user/**").hasAnyRole("USER", "ADMIN")
+                //Все остальные страницы требуют авторизацию
+                .anyRequest().authenticated()
+                .and()
+                //Разрешить доступ к форме логина для всех и применить хендлер
+                .formLogin().successHandler(successUserHandler)
+                .permitAll()
+                .and()
+                //Логаут разрешен всем
+                .logout()
+                .permitAll();
+
+        return http.build();
     }
 
 
